4ce87bdbd1775ce08449faa203072095cc5c199a
Jaswant-Singh-Agore <jaswantagore@gmail.com>
2025-10-26 18:10:21 +0530

    Updated gitignore file

--- application.py
+++ application.py
@@ -43,8 +43,9 @@ def predict_datapoint():
         return render_template('home.html',results=results[0])
     
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+if __name__ == "__main__":
+    application.run(debug=True)
+
        
 
 
